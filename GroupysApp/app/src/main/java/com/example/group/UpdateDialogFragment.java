44445f9dc8c921b04fa2213a6a9573ae701a512b
gu1Vaz <guilima233@gmail.com>
2023-08-09 10:58:26 -0300

    dialog fix

--- GroupysApp/app/src/main/java/com/example/group/UpdateDialogFragment.java
+++ GroupysApp/app/src/main/java/com/example/group/UpdateDialogFragment.java
@@ -3,7 +3,13 @@ package com.example.group;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
@@ -15,11 +21,33 @@ public class UpdateDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle(R.string.update_title)
                 .setMessage(R.string.update_message)
-                .setPositiveButton(R.string.update_button, update);
-        return builder.create();
+                .setPositiveButton(R.string.update_button, null)
+                .setCancelable(false);
+
+        AlertDialog dialog = builder.create();
+
+        dialog.setOnShowListener(dialogInterface -> {
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setBackgroundColor(Color.TRANSPARENT);
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.parseColor("#FF6666"));
+            dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(update);
+        });
+        dialog.setCanceledOnTouchOutside(false);
+        return dialog;
     }
 
-    DialogInterface.OnClickListener update = (dialog, which) -> {
-
+    private final View.OnClickListener update = v -> {
+        openPlayStore();
     };
+
+    private void openPlayStore() {
+        String packageName = "com.example.yourapp";
+        try {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName));
+            startActivity(intent);
+        } catch (android.content.ActivityNotFoundException anfe) {
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + packageName));
+            startActivity(intent);
+        }
+    }
 }
+
